6e49de9227255fe2466a01b8ec930cbee289a926
OhMimi <gash60535@gmail.com>
2020-05-20 00:44:44 +0800

    [update]single link

---
+++ singlelink/main.go
@@ -0,0 +1,123 @@
+package main
+
+import "fmt"
+
+type HeroNode struct {
+	no       int
+	name     string
+	nickname string
+	next     *HeroNode // 這裡表示指向的下一個節點
+}
+
+// 給練表插入一個節點
+// 編寫第一種插入方式,在單鍊表的最後加入
+func InsertHeroNode(head, newHeroNode *HeroNode) {
+	// 思路
+	// 1. 先找到該鍊表的最後的節點
+	// 2. 創建一個輔助節點
+	temp := head
+	for {
+		if temp.next == nil { // 表示找到最後
+			break
+		} else {
+			temp = temp.next // 讓temp不斷地指向下一個節點
+		}
+	}
+
+	// 3. 將newHeroNode加入到鍊表的最後
+	temp.next = newHeroNode
+}
+
+// 給練表插入一個節點
+// 編寫第二種插入方式,在單鍊表的最後加入
+func InsertHeroNode2(head, newHeroNode *HeroNode) {
+	// 思路
+	// 1. 先找到該鍊表的最後的節點
+	// 2. 創建一個輔助節點
+	temp := head
+	flag := false
+	for {
+		if temp.next == nil { // 表示找到最後
+			break
+		} else if temp.next.no > newHeroNode.no {
+			// 說明newHeroNode此時需插入到temp之後
+			break
+		} else if temp.next.no == newHeroNode.no {
+			// 說明鍊表中已有no號存在，無法插入
+			flag = true
+			break
+		} else {
+			temp = temp.next
+		}
+	}
+
+	if flag {
+		fmt.Printf("no號已存在不允許插入, no = %d\n", newHeroNode.no)
+		return
+	} else {
+		newHeroNode.next = temp.next
+		temp.next = newHeroNode
+	}
+}
+
+// 顯示鍊表的所有節點信息
+func ListHeroNode(head *HeroNode) {
+	// 1. 創建一個輔助節點
+	temp := head
+
+	// 先判斷該鍊表是否為空鍊表
+	if temp.next == nil {
+		fmt.Println("空鍊表無法顯示")
+		return
+	}
+
+	// 2. 遍歷這個鍊表
+	for {
+		fmt.Printf("[%d , %s , %s] ==> ", temp.next.no, temp.next.name, temp.next.nickname)
+		temp = temp.next
+		// 判斷是否為鍊表最後
+		if temp.next == nil {
+			break
+		}
+	}
+}
+
+func main() {
+
+	// 1. 先創建一個頭節點,
+	head := &HeroNode{}
+
+	// 2. 創建一個新的節點
+	hero1 := &HeroNode{
+		no:       1,
+		name:     "hulk",
+		nickname: "popopo",
+	}
+
+	hero2 := &HeroNode{
+		no:       2,
+		name:     "tom",
+		nickname: "tommy",
+	}
+
+	hero3 := &HeroNode{
+		no:       3,
+		name:     "tony",
+		nickname: "tony123",
+	}
+
+	hero4 := &HeroNode{
+		no:       4,
+		name:     "appaa",
+		nickname: "appaa123",
+	}
+
+	// 3. 加入
+	InsertHeroNode2(head, hero3)
+	InsertHeroNode2(head, hero4)
+	InsertHeroNode2(head, hero1)
+	InsertHeroNode2(head, hero2)
+
+	// 4. 顯示
+	ListHeroNode(head)
+}
