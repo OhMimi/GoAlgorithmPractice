cc4dea6b2ce359d39eac42123f65a5c7c819161f
OhMimi <gash60535@gmail.com>
2020-05-25 00:45:08 +0800

    [update] Josephu practice2

--- Josephu/main.go
+++ Josephu/main.go
@@ -64,8 +64,62 @@ func ShowBoy(first *Boy) {
 	}
 }
 
+/*
+設編號1, 2, ... n 的n個人坐一圈,約定編號為k (1<=startNo<=n)
+的人從1開始報數,數到countNum的那個人出列,他的下一個又從1開始報數,
+數到m的那個人又出列，依次類推,直到所有人出列為止,由此生產出一個出隊編號的序列
+*/
+
+// 分析思路
+// 1. 編寫一個函數,PlayGame(first *Boy, startNo int, countNum int)
+// 2. 最後使用一個算法,按照要求,在環形鍊表中留下最後一個人即完成
+func PlayGame(first *Boy, startNo, countNum int) {
+
+	// 1. 空的鍊表我們單獨處理
+	if first.Next == nil {
+		fmt.Println("鍊表為空,沒有小孩")
+		return
+	}
+	// 留一個,判斷 startNo <= 小孩的總數
+	// 2. 需要定義一個輔助節點,幫助我們刪除小孩
+	tail := first
+	// 3. 讓tail執行環形鍊表的最後一個小孩,非常重要
+	// 因為tail在刪除小孩時會需要用到
+	for {
+		if tail.Next == first { // 代表已經到最後一個小孩了
+			break
+		}
+		tail = tail.Next
+	}
+	// 4. 讓first移動到startNo [後面刪除小孩,就以first為準]
+	for i := 1; i <= startNo-1; i++ {
+		first = first.Next
+		tail = tail.Next
+	}
+	// 5. 開始數 countNum, 然後刪除first指向的小孩
+	for {
+		// 開始數countNum-1次
+		for i := 1; i <= countNum-1; i++ {
+			first = first.Next
+			tail = tail.Next
+		}
+		fmt.Printf("小孩編號為 %d 出列 ->\n", first.No)
+		// 刪除first指向的節點
+		first = first.Next
+		tail.Next = first
+
+		// 判斷如果tail == first 圈中只剩下一個
+		if tail == first {
+			break
+		}
+	}
+	fmt.Printf("最後小孩編號為 %d 出列 ->\n", first.No)
+}
+
 func main() {
 	first := AddBoy(5)
 	// 顯示
 	ShowBoy(first)
+
+	PlayGame(first, 2, 3)
 }
